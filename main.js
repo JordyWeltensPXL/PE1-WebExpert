b2e1398f980c9b5c0e47f662af22ade14848ccd1
JordyWeltensPXL <92799790+JordyWeltensPXL@users.noreply.github.com>
2023-09-25 23:56:07 +0200

    afwerken nav, main en product in de kijker

---
+++ main.js
@@ -0,0 +1,15 @@
+function addBounce(e) {
+  e.target.classList.add("fa-bounce");
+}
+
+function removeBounce(e) {
+  e.target.classList.remove("fa-bounce");
+}
+
+function addFade(e) {
+  e.target.classList.add("fa-fade");
+}
+
+function removeFade(e) {
+  e.target.classList.remove("fa-fade");
+}
